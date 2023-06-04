e7927642778040db436e0412d823d802f555f417
Aaron Chacon <aaronch.dev@gmail.com>
2023-06-04 17:56:49 -0300

    feat: navbar, and hero init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+// import Footer from "./components/Footer";
 
 function App() {
-  return <>App</>;
+  return (
+    <Box width="100%" sw={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      {/* <Footer /> */}
+    </Box>
+  );
 }
 
 export default App;
